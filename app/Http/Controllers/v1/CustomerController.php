10a53656406910b45fdef95fbfcab28ec1269606
Samson Ogheneriye Eduotu <ogheneinfinitea@gmail.com>
2023-04-10 02:05:40 +0100

    created requests to handle customer requests

--- app/Http/Controllers/v1/CustomerController.php
+++ app/Http/Controllers/v1/CustomerController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public  function test() {
+        return "this is test";
+    }
+
+    public  function createCustomer(Request $request){
+
+
+
+        $customer = new Customer();
+        $customer->full_name = $request->full_name;
+        $customer->email = $request->email;
+        $customer->phone_number = $request->phone_number;
+
+        $customer->save();
+
+        return response()->json([
+            'message' => 'Customer created successfully'
+        ]);
+
+    }
+
+    public  function getAllCustomers(){
+        $customers = Customer::all();
+
+        if($customers->isEmpty())
+            return response()->json([
+                'message' => 'No customers found'
+            ], 404);
+
+
+        return response()->json([
+            'customers' => $customers
+        ]);
+    }
 }
